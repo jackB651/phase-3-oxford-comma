2d9754a19a4453eef9cccc28455242a062c6b244
Jonathon <jwbialas@gmail.com>
2022-08-30 13:14:57 -0500

    fin

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+  if array.length() == 1
+    "#{array[0]}"
+  elsif array.length() == 2
+    "#{array.join(" and ")}"
+  else 
+     i = 0 
+     a = []
+     while i < (array.length() - 1)
+        a.push(array[i])
+        i += 1
+     end
+     b = a.join(", ")
+     d = []
+     d.push(b)
+     d.push(", ")
+     f = d.join
+     c = []
+     c.push(f)
+     c.push(array[-1])
+     c.join("and ")
+  end  
 end
