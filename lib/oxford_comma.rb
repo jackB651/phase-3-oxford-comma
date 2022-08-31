7a2e15f3380ee2848321da698ccad87e9959f262
Jonathon <jwbialas@gmail.com>
2022-08-30 23:27:34 -0500

    commit tree 92c7ae548ea5bc44f25b60973f4e873afec52c22

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,20 +4,29 @@ def oxford_comma(array)
   elsif array.length() == 2
     "#{array.join(" and ")}"
   else 
-     i = 0 
-     a = []
-     while i < (array.length() - 1)
-        a.push(array[i])
-        i += 1
-     end
-     b = a.join(", ")
-     d = []
-     d.push(b)
-     d.push(", ")
-     f = d.join
-     c = []
-     c.push(f)
-     c.push(array[-1])
-     c.join("and ")
+    #  i = 0 
+    #  a = []
+    #  while i < (array.length() - 1)
+    #     a.push(array[i])
+    #     i += 1
+    #  end
+    #  b = a.join(", ")
+    #  d = []
+    #  d.push(b)
+    #  d.push(", ")
+    #  f = d.join
+    #  c = []
+    #  c.push(f)
+    #  c.push(array[-1])
+    #  c.join("and ")
+
+    i = 0
+    a = []
+    while i <array.length
+      a[i] = "#{array[i]}, "
+      i += 1
+    end
+    a[-1] = "and #{array[-1]}"
+    a.join
   end  
 end
